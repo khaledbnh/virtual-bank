9c4015ca3859a2396d005f3af2969b6f9fbf10d7
AzharB <bejaouiazhar45@gmail.com>
2022-07-13 10:56:33 +0100

    feat(): Adding client entity

---
+++ src/main/java/tn/esprit/entities/Client.java
@@ -0,0 +1,26 @@
+package tn.esprit.entities;
+
+
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Data
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "T_CLIENT")
+public class Client {
+
+	@Column(name="nom")
+	private String nom;
+}
